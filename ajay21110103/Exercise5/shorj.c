a268cf44224062028e376ab4cd4733cc5abf17dc
root <92035508+AjayBadrinath@users.noreply.github.com>
2023-02-03 02:11:46 +0530

    commit SJF

--- ajay21110103/Exercise5/shorj.c
+++ ajay21110103/Exercise5/shorj.c
@@ -35,7 +35,9 @@ int main(){
 	}
 
 	int visited_pid[num],n;
-	int min=proctable[0][1],v=0,ct=0,k=0,minidx;
+	int min=proctable[0][1],v=0,ct=0,k=0,minidx,tat,wt;
+	float ct_avg=0.0,tat_avg=0.0,wt_avg=0.0;
+	
 	for(int j=0;j<num;j++){
 			if(proctable[j][1]<=min&& is_vis(visited_pid,proctable[j][0],v)==0){
 				min=proctable[j][1];
@@ -45,8 +47,13 @@ int main(){
 		}
 	visited_pid[v++]=proctable[minidx][0];
 	ct+=proctable[minidx][2];
+	tat=ct-proctable[minidx][1];
+	wt=tat-proctable[minidx][2];
+	ct_avg+=(float)ct;
+	tat_avg+=(float)tat;
+	wt_avg+=(float)wt;
+	printf("PID:%d \t CT:%d\t TAT:%d\t WT:%d\n",proctable[minidx][0],ct,tat,wt);
 	
-	//for(int i=0;i<num;i++){
 	while(v<num){
 		
 		min=10000;
@@ -60,25 +67,17 @@ int main(){
 		}
 		visited_pid[v++]=proctable[minidx][0];
 		ct+=proctable[minidx][2];
-		/*
-		min=10000;
-		for(int j=0;j<num;j++){
-			if(proctable[j][1]<=ct&& is_vis(visited_pid,proctable[j][0],v)==0){
-				if(proctable[j][2]<min){
-				min=proctable[j][2];
-
-				minidx=j;
-			}
-			}
-		}
-		visited_pid[v++]=proctable[minidx][0];
-		ct+=proctable[minidx][2];
-		*/
+		tat=ct-proctable[minidx][1];
+		wt=tat-proctable[minidx][2];
+		ct_avg+=(float)ct;
+		tat_avg+=(float)tat;
+		wt_avg+=(float)wt;
+		printf("PID:%d \t CT:%d\t TAT:%d\t WT:%d\n",proctable[minidx][0],ct,tat,wt);
+		
 	}
+	printf("Average CT :%.2f \t Average TAT:%.2f \t Average WT:%.2f \n",ct_avg/(float)num,tat_avg/(float)num,wt_avg/(float)num);
+	//for(int i=0;i<v;i++){
+	//	printf("\n%d\t",visited_pid[i]);
 	//}
-
-	for(int i=0;i<v;i++){
-		printf("\n%d\t",visited_pid[i]);
-	}
-	printf("\n%d",ct);
+	
 }
