0e73ba75c6c972a3078966a4a1d2fa00b304334a
root <92035508+AjayBadrinath@users.noreply.github.com>
2023-02-03 15:36:45 +0530

    Initial Commit

---
+++ ajay21110103/Exercise5/SRTF.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a ,int *b){
+	int tmp=*a;
+	*a=*b;
+	*b=tmp;
+}
+int is_vis(int *vis,int pid,int vislen){
+	for(int i=0;i<vislen;i++){if(pid==vis[i]){return 1;}}
+	return 0;
+}
+void sort(int proc[][3],int len){
+	for(int i=0;i<len;i++){
+		for(int j=0;j<len-i-1;j++){
+			if(proc[j][1]>proc[j+1][1]){
+				swap(&proc[j][0],&proc[j+1][0]);
+				swap(&proc[j][1],&proc[j+1][1]);
+				swap(&proc[j][2],&proc[j+1][2]);
+			}
+		}
+	}
+	 
+}
+int is_burst_zero(int proc[][3],int len){
+	int rem_len=0;
+	for(int i=0;i<len;i++){
+		if(proc[i][2]!=0){
+			rem_len++;
+		}
+	}
+	if(rem_len==0){
+		return 0;
+	}
+	return 1;
+	
+}
+int main(){
+	int num,bt,at,pid;
+	printf("Enter the number of Processes:"); 
+	scanf("%d",&num);
+	int proctable[num][3],n,min=proctable[0][1],minidx;
+	int dup[num][3];
+	int vis[num];
+	for(int i=0;i<num;i++){
+
+		scanf("%d %d %d",&pid,&at ,&bt);
+		dup[i][0]=pid;
+		dup[i][1]=at;
+		dup[i][2]=bt;
+		proctable[i][0]=pid;
+		proctable[i][1]=at;
+		proctable[i][2]=bt;
+	}
+	sort(proctable,num);
+	sort(dup,num);
+	int proj_c=0;
+	/*
+	printf("\n");
+	for(int i=0;i<num;i++){
+		
+			printf("%d %d %d \n",proctable[i][0],proctable[i][1],proctable[i][2]);
+
+		
+	}
+	*/
+	int v=0;
+	int arr[num*num];
+	int i=0,f=0;
+	int ct=0,c_proj=0;
+	int g=0;
+	int zc=0;
+	int ct1[num*num];
+	while(zc<num){
+		//printf("\n%d\n",ct);
+		//printf("PID:%d \t CT :%d \t TAT:%d\t WT:%d \n",proctable[i][0],ct-proctable[i][1]);
+		if(proctable[i][2]==0){
+			ct1[f++]=ct;
+			zc++;
+			i++;
+			continue;
+		}
+		if(i==num){
+			i=0;
+			continue;
+		}
+		if(is_vis(vis,proctable[i][0],v)==1){
+			ct1[f++]=ct;
+			ct+=proctable[i][2];
+			proctable[i][2]=0;
+
+			arr[g++]=proctable[i][0];
+			i++;
+			continue;
+		}
+		if(proctable[i+1][2]<proctable[i][2]){
+			ct+=proctable[i+1][1]-proctable[i][1];
+			proctable[i][2]--;
+			arr[g++]=proctable[i][0];
+			vis[v++]=proctable[i][0];
+		}
+		else{
+			ct1[f++]=ct;
+			ct+=proctable[i][2];
+			arr[g++]=proctable[i][0];
+
+			proctable[i][2]=0;
+		}
+		i++;
+	}
+	//printf("%d",ct);
+
+
+	for(int i=0;i<g;i++){
+		printf("PID:%d \t CT :%d \t TAT:%d\t WT:%d \n",arr[i],ct1[i],ct1[i]-dup[arr[i]][1],(ct1[i]-dup[arr[i]][1])-dup[arr[i]][2]);
+		
+	}
+
+}
